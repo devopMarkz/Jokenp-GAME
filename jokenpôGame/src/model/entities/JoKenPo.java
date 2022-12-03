0dfc97c919962bf5333dc349728a70ad9faf183a
Marcos André <117694734+devopMarkz@users.noreply.github.com>
2022-12-03 16:25:30 -0300

    Excluí fechamento de Scanner para evitar NoSuchFoundExcep

--- jokenpôGame/src/model/entities/JoKenPo.java
+++ jokenpôGame/src/model/entities/JoKenPo.java
@@ -1,6 +1,5 @@
 package model.entities;
 
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 // Classe que contém a lógica do jogo
@@ -26,26 +25,18 @@ public record JoKenPo(
 		
 	}
 	
-	// Método responsável pela escolha da jogada do player (Não consigo resolver a exceção)
+	// Método responsável pela escolha da jogada do Player user
 	private int userChoice () {
-		try {
+
 		Scanner sc = new Scanner (System.in);
 		
-		System.out.println("1 - PEDR A");
-		System.out.println("2 - PAPEL ");
-		System.out.println("3 - TESOURA ");
+		System.out.println("1 - PEDRA");
+		System.out.println("2 - PAPEL");
+		System.out.println("3 - TESOURA");
 		System.out.print("Informe sua jogada: ");
 		int userChoice = sc.nextInt();
 		
-		sc.close();
-		
 		return userChoice;
-		}
-		catch (NoSuchElementException exceptionInLine38) {
-			System.out.println("Help me");
-			exceptionInLine38.printStackTrace();
-			return 0;
-		}
 
 	}
 }
